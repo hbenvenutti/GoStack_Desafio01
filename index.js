4248005c87991983714d09c464d62d4094b3aec2
Huam Benvenutti <huambenvenutti@protonmail.com>
2020-02-02 19:48:28 -0300

    feature: Routes writed the delete route; had to create a function to search the id in the array;

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = express();
 
 server.use(express.json());
 
-const projects = ['teste'];
+const projects = [];
 
 
 server.get('/projects', (req, res) => {
@@ -12,16 +12,41 @@ server.get('/projects', (req, res) => {
 });
 
 server.post('/projects', (req,res) => {
-    const { id, tasks} = req.body;
+    const { id, title, tasks} = req.body;
     
     const project = {
         id: id,
+        title: title,
         tasks: tasks,
     }
 
     projects.push(project);
+
+    return res.json(projects);
+});
+
+
+function searchInArray(array, id){
+    const len = array.length;
+
+    for (var i=0; i<len; i++){
+        if (array[i].id === id){
+            return i;
+        };
+    };
+
+    return -1;
+
+};
+
+server.delete('/projects/:id', (req, res) => {
+    const { id } = req.params;
     
-    return res.send("POST");
+    const index = searchInArray(projects, id);
+    
+    projects.splice(index, 1);
+
+    return res.json(projects);
 });
 
 
